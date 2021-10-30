25129efc7f7792aa40f580600a110a4b64e47a5e
GordenkoAndrey <andreygordenko3002@gmail.com>
2021-10-30 18:13:07 +0300

    added task 1

---
+++ Lesson_3/task_1/task_1.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <random>
+using namespace std;
+
+int main()
+{
+	int arr[10];
+	int num = 0;
+	
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "Enter please array elements: ";
+		cin >> arr[i];
+	}
+	cout << endl;
+	cout << "Enter please number: ";
+	cin >> num;
+	cout << endl;
+
+	for (int i = 0; i < 10; i++)
+	{
+		arr[i] += num;
+	}
+	cout << "Your numbers: " << endl;
+	for (int i = 0; i < 10; i++)
+	{
+		cout << arr[i] << endl;
+	}
+	cout << endl;
+}

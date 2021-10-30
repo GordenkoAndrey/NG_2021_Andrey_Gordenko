e75b6452818fc6bbd833f4cbb38a86530f10a75b
GordenkoAndrey <andreygordenko3002@gmail.com>
2021-10-30 17:13:42 +0300

    fixed code

--- Lesson_1/task_6/task_6.cpp
+++ Lesson_1/task_6/task_6.cpp
@@ -12,25 +12,15 @@ int main()
 
 	switch (Mask)
 	{
-	case 255: cout << "Number of available ip-addresses: 1" << endl;
-		break;
-	case 254: cout << "Number of available ip-addresses: 2" << endl;
-		break;
-	case 252: cout << "Number of available ip-addresses: 4" << endl;
-		break;
-	case 248: cout << "Number of available ip-addresses: 8" << endl;
-		break;
-	case 240: cout << "Number of available ip-addresses: 16" << endl;
-		break;
-	case 224: cout << "Number of available ip-addresses: 32" << endl;
-		break;
-	case 192: cout << "Number of available ip-addresses: 64" << endl;
-		break;
-	case 128: cout << "Number of available ip-addresses: 128" << endl;
-		break;
-	case 000: cout << "Number of available ip-addresses: 256" << endl;
-		break;
-	default: cout << "Wrong number, please try again)" << endl;
-		break;
+	case 255: cout << "Number of available ip-addresses: 1" << endl; break;
+	case 254: cout << "Number of available ip-addresses: 2" << endl; break;
+	case 252: cout << "Number of available ip-addresses: 4" << endl; break;
+	case 248: cout << "Number of available ip-addresses: 8" << endl; break;
+	case 240: cout << "Number of available ip-addresses: 16" << endl; break;
+	case 224: cout << "Number of available ip-addresses: 32" << endl; break;
+	case 192: cout << "Number of available ip-addresses: 64" << endl; break;
+	case 128: cout << "Number of available ip-addresses: 128" << endl; break;
+	case 000: cout << "Number of available ip-addresses: 256" << endl; break;
+	default: cout << "Wrong number, please try again)" << endl; break;
 	}
 }

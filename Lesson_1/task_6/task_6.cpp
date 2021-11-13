4e7b70bbfc24a9ec4d419e96756e5b4fcbc23261
GordenkoAndey <andreygordenko3002@gmail.com>
2021-11-14 00:44:18 +0200

    fixed task 6

--- Lesson_1/task_6/task_6.cpp
+++ Lesson_1/task_6/task_6.cpp
@@ -5,22 +5,22 @@ int main()
 {
 	int Mask = 0;
 
-	cout << "Netmask: 255.255.255.*" << endl
-		<< "Enter * : ";
+	cout << "Netmask: 255.255.255.*" << endl << "Enter * : ";
 	cin >> Mask;
 	cout << endl;
+	cout << "Available ip-addresses: ";
 
 	switch (Mask)
 	{
-	case 255: cout << "Number of available ip-addresses: 1" << endl; break;
-	case 254: cout << "Number of available ip-addresses: 2" << endl; break;
-	case 252: cout << "Number of available ip-addresses: 4" << endl; break;
-	case 248: cout << "Number of available ip-addresses: 8" << endl; break;
-	case 240: cout << "Number of available ip-addresses: 16" << endl; break;
-	case 224: cout << "Number of available ip-addresses: 32" << endl; break;
-	case 192: cout << "Number of available ip-addresses: 64" << endl; break;
-	case 128: cout << "Number of available ip-addresses: 128" << endl; break;
-	case 000: cout << "Number of available ip-addresses: 256" << endl; break;
-	default: cout << "Wrong number, please try again)" << endl; break;
+	case 255: cout << "1" << endl; break;
+	case 254: cout << "2" << endl; break;
+	case 252: cout << "4" << endl; break;
+	case 248: cout << "8" << endl; break;
+	case 240: cout << "16" << endl; break;
+	case 224: cout << "32" << endl; break;
+	case 192: cout << "64" << endl; break;
+	case 128: cout << "128" << endl; break;
+	case 000: cout << "256" << endl; break;
+	default: cout << "not found)" << endl; break;
 	}
 }

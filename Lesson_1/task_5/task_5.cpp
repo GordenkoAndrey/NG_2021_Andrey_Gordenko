e382e6e31854863721826cd9ec719a9cc7aba21b
GordenkoAndey <andreygordenko3002@gmail.com>
2021-11-15 20:25:46 +0200

    fixed task 5

--- Lesson_1/task_5/task_5.cpp
+++ Lesson_1/task_5/task_5.cpp
@@ -4,10 +4,7 @@ using namespace std;
 int main()
 {
 	double ship_mass = 0;
-	double required_fuel = 0;
 	cout << "Enter please mass ship: ";
 	cin >> ship_mass;
-	cout << endl;
-	required_fuel = (ship_mass / 3 - 2) * 300;
-	cout << "Amount of fuel needed: " << required_fuel << endl;
+	cout << endl << "Amount of fuel needed: " << (ship_mass / 3 - 2) * 300 << endl;
 }

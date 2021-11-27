87dfdf30217c3d0b14d727e4437849180a9efe7d
GordenkoAndey <andreygordenko3002@gmail.com>
2021-11-27 23:58:37 +0200

    added task 5

---
+++ Lesson_4/task_5/task_5.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char input[256];
+
+	cout << "Enter please text: ";
+	cin.getline(input, 256);
+	cout << endl;
+
+	for (int i = 0; input[i] != '\0' ; i++)
+	{
+		if (input[i] == 'f' && input[i + 1] == 'u' &&
+			input[i + 2] == 'c' && input[i + 3] == 'k')
+		{
+			input[i] = '*';
+			input[i + 1] = '*';
+			input[i + 2] = '*';
+			input[i + 3] = '*';
+		}
+	}
+	for (int i = 0; input[i] != '\0'; i++)
+	{
+		cout << input[i];
+	}
+}

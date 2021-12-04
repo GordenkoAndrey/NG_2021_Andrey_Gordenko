6129b02cc96d70c45be73c179bff49d354f7c1ac
GordenkoAndey <andreygordenko3002@gmail.com>
2021-12-04 23:58:38 +0200

    added task 5

--- Lesson_4/task_5/task_5.cpp
+++ Lesson_4/task_5/task_5.cpp
@@ -15,10 +15,10 @@ int main()
 		if (input[i] == 'f' && input[i + 1] == 'u' &&
 			input[i + 2] == 'c' && input[i + 3] == 'k')
 		{
-			input[i] = '*';
-			input[i + 1] = '*';
-			input[i + 2] = '*';
-			input[i + 3] = '*';
+			for (int j = i; j < i + 4; j++)
+			{
+				input[j] = '*';
+			}
 		}
 	}
 	for (int i = 0; input[i] != '\0'; i++)

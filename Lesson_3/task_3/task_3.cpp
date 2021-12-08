a8076328a52ccc98eb82a88970493588c92d529d
GordenkoAndey <andreygordenko3002@gmail.com>
2021-12-09 00:19:33 +0200

    added task 3

--- Lesson_3/task_3/task_3.cpp
+++ Lesson_3/task_3/task_3.cpp
@@ -10,23 +10,14 @@ int main()
  	int arr1[4];
 	int arr2[4];
 	int sum = 0;
-	int num1 = 0;
-	int num2 = 0;
 	int mull = 1000;
 
 	for (int i = 0; i < 4; i++)
 	{
 		arr1[i] = rand() % 10;
 		arr2[i] = rand() % 10;
-	}
-	for (int i = 0; i < 4; i++)
-	{
-		arr1[i] *= mull;
-		arr2[i] *= mull;
+		sum += (arr1[i] * mull) + (arr2[i] * mull);
 		mull /= 10;
-		num1 += arr1[i];
-		num2 += arr2[i];
 	}
-	sum = num1 + num2;
-	cout <<"sum= "<< sum << endl;
+	cout << "Sum = " << sum << endl;
 }

ffa19f90addc219044cabd5882dae01fc6030abd
GordenkoAndey <andreygordenko3002@gmail.com>
2021-12-12 00:10:43 +0200

    added task 3

---
+++ Lesson_4/task_3/task_3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    char text[] = { "yt ufxx ymnx yfxp fii dtzw  fxsfrj htrrjsy bnym qnsp yt uwtlwfr ns lny" };
+    for (int i = 0; i < sizeof(text); i++)
+    {
+        if (text[i] != ' ') 
+        {
+            text[i] -= 5;
+        }
+        cout << text[i];
+    }
+}
